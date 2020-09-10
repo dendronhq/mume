99ee1278443434702ef3d8899ba178c498b01ce1
kevinslin <kevin@thence.io>
2020-09-10 15:32:04 -0700

    enhance: support wildcard refs

--- test/lib/dendron.test.ts
+++ test/lib/dendron.test.ts
@@ -100,4 +100,37 @@ describe("MarkdownEngine", () => {
     expect(out.html.indexOf("h1 header") < 0).toBeTruthy();
     expect(out.html.indexOf("h2 header") < 0).toBeTruthy();
   });
+
+  test("with wildcard", async () => {
+    const configPath = path.resolve(os.homedir(), ".mume"); // use here your own config folder, default is "~/.mume"
+    await mume.init(configPath); // default uses "~/.mume"
+    const engine = new mume.MarkdownEngine({
+      filePath: "./test/integration/fixtures/dendron/ref.md",
+      projectDirectoryPath: "./test/integration/fixtures/dendron",
+      config: {
+        configPath: configPath,
+        previewTheme: "github-light.css",
+        // revealjsTheme: "white.css"
+        codeBlockTheme: "default.css",
+        printBackground: true,
+        enableScriptExecution: true, // <= for running code chunks
+      },
+    });
+    const out = await engine.parseMD(
+      [
+        "((ref:[[ref]]#h1 header,1:#*))",
+        "---",
+        "",
+        "`code span`" + "some text",
+      ].join("\n"),
+      {
+        isForPreview: false,
+        useRelativeFilePath: false,
+        hideFrontMatter: false,
+      },
+    );
+    expect(out.html).toMatchSnapshot();
+    expect(out.html.indexOf("h1 header") < 0).toBeTruthy();
+    expect(out.html.indexOf("h2 header") < 0).toBeTruthy();
+  });
 });
